655de80212cb113dd1ec2e710f3e942427a21f84
Ericka Angeles <ericka.angelesgarcia@gmail.com>
2022-11-09 13:34:26 +0100

    first exercise

--- ericka/index.php
+++ ericka/index.php
@@ -1,12 +1,27 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php
+// exercise 1
+$i=0;
+while($i<51)
+{echo "$i name,";
+$i++;}
+// exercise 2
+$numbers=[1,24,33,4,57,6,78,8,9,10];
+foreach($numbers as $value)
+       {
+           echo "Value is $value <br/>";
+       }
+    //    exercise 3
+    function calc ($number){
+       echo max($number);
+    }
+    // calc($numbers);
+
+    // exercise 4
+for ($i=0; $i<50; $i++){
+    if($i % 5 == 0 ){
+        echo " $i is multiple of 5 <br>";
+    }  
+    else {
+        echo "$i  <br>";
+    }
+}

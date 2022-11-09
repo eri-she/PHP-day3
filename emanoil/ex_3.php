4f56e8b7d74126b0738a34c6ef646d0d04d93557
Emanoil Karov <e_karov@yahoo.com>
2022-11-09 12:53:11 +0100

    Added ex_3

---
+++ emanoil/ex_3.php
@@ -0,0 +1,23 @@
+<?php
+$arr = [];
+
+for ($i=0; $i < 20; $i+=2) { 
+    array_push($arr, rand(-1000, 1000));
+}
+
+echo "<pre>";
+var_dump($arr);
+echo "</pre>";
+
+function getMaxNumber($arr){
+    $maxNumber = $arr[0];
+    foreach ($arr as $number) {
+        if ($number > $maxNumber) {
+            $maxNumber = $number;
+        }
+    }
+    return $maxNumber;
+}
+
+$highestValue = getMaxNumber($arr);
+echo "The heighest value in the array is: $highestValue";
